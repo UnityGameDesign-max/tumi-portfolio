7ebeacce8ed3cd03c50b0755a866e3c0fb5674a6
Tumelo <tumie201511@gmail.com>
2021-02-28 19:47:57 +0200

    updated Project js file

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -9,9 +9,9 @@ function Projects() {
             <p className="projects__paragraph">Welcome to TumiDev's projects</p>
 
             <div className="project__row">
-                <ProjectCard projectName="Amazon Clone" finishedDate="invalid" company="None" type="personal" link="#"/>
-                <ProjectCard projectName="Instagram Clone" finishedDate="invalid" company="None" type="personal" link="#"/>
-                <ProjectCard projectName="Accounting Software" finishedDate="invalid" company="IBS " type="IBS" link="#"/>
+                <ProjectCard projectName="Amazon Clone" finishedDate="invalid" description="This is a project I did to represent skills" company="None" type="personal" link="#"/>
+                <ProjectCard projectName="Instagram Clone" finishedDate="invalid" description="This is a project I did to represent skills" company="None" type="personal" link="#"/>
+                <ProjectCard projectName="Accounting Software" finishedDate="invalid" description="This is a Business project for IBS" company="IBS " type="IBS" link="#"/>
             </div>
             
         </div>
