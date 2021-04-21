e522fbc9a277dad4326791f5dfe819001cb3ec90
Tumelo <tumie201511@gmail.com>
2021-04-21 13:50:18 +0200

    added a component for the Resume

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Skills from './Skills/Skills'
 import Projects from './Projects/Projects'
 import About from './About/About'
 import Contacts from './Contacts/Contacts'
+import Resume from './Resume/Resume';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Skills />
       <Projects />
       <About />
+      <Resume />
       <Contacts />
       
       
