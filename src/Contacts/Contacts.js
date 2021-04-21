5607ebd28876380c4f8b1572169cc2c30dd31036
Tumelo <tumie201511@gmail.com>
2021-04-21 03:45:43 +0200

    added Contacts js file for adding new fields from material-ui

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -1,45 +1,68 @@
 import React from 'react'
 import './Contacts.css'
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
 import EmailIcon from '@material-ui/icons/Email';
 import PhoneIcon from '@material-ui/icons/Phone';
 import { IconName } from "react-icons/fc";
+import {makeStyles} from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import { Button, IconButton } from '@material-ui/core';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
 
 
 
 
-const styles = {
-    largeIcon: {
-        width: 60,
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
     },
-};
+}))
 
 function Contacts() {
+    const classes = useStyles();
 
     return (
         <div className="contacts">
             <h1 className="contacts__bgText">Contacts</h1>
             <div className="all__contacts__icons">
                 <div className="column__contacts">
-                    <div className="col__1">
-                        <a href="https://github.com/UnityGameDesign-max" ><GitHubIcon className="contacts__icons__github"  fontSize="large" /></a>
-                        <LinkedInIcon className="contacts__icons__linkedin" fontSize="large" />
-                    </div>
-                    
-                    <div className="col__2">
-                        <div className="email__address">
-                            <EmailIcon className="contacts__icons__email" fontSize="large" />
-                            <small>tumie201511@gmail.com</small>
+                    <form className={classes.root} noValidate autoComplete="off">
+                        
+                        <TextField id="filled-basic" label="Name" variant="filled" />
+                        <TextField id="filled-basic" label="Email" variant="filled" />
+                        <div className="row__text">
+                            
+                            <TextField id="filled-basic" multiline rows={5} columns={5} label="Message" variant="filled" />
                         </div>
-                
-                        <div className="phone__number">
-                            <PhoneIcon className="contacts__icons__phone" fontSize="large" />
-                            <small>+27 692 92602</small>
-                        </div>
-                    </div>
+
+                        <Button variant="outlined" color="primary">Send Message!</Button>
+                    </form>
+                </div>
+
+                <div className="contact__icons">
+                    <IconButton>
+                        <a href="https:github.com/UnityGameDesign-max">
+                            <GitHubIcon className="github__icons" />
+                        </a>
+                    </IconButton>
+                    <IconButton>
+                        <a href="#">
+                            <LinkedInIcon className="linkedin__icons" />    
+                        </a>
+                    </IconButton>
                     
+                    <IconButton>
+                        <a href="#">
+                            <TwitterIcon className="twitter__icons" />
+                        </a>
+                    </IconButton>
                     
+
                 </div>
             </div>
             
