4b8f58da269f35d88f3752cba3f3e45e5cbed109
Tumelo <tumie201511@gmail.com>
2021-04-21 03:51:01 +0200

    added new icon

--- src/HeroSection/HeroSection.js
+++ src/HeroSection/HeroSection.js
@@ -1,30 +1,34 @@
 import React from 'react'
 import './HeroSection.css'
 import Typical from 'react-typical'
-import HeroBg from '../HeroBg/HeroBg'
-import  Player  from 'video-react';
+
 
 
 function HeroSection() {
     return (
         <div className="hero__section">
+            <div className="hero__img">
+                <img className="hero__section__img" src={process.env.PUBLIC_URL + "HtmlCssJs.svg" }  alt=""/>
+            </div>
 
-            <img className="hero__section__img" src={process.env.PUBLIC_URL + "server__svg.png" }  alt=""/>
-            
-            <h1 className="hero__section__text">Real World Software and Web Applications</h1>
+            <div className="hero__text">
+                <h1 className="hero__section__text">Real World Web Applications!</h1>
 
-            <h3 className="intro__text">
-                <Typical 
-                steps={['I am an enthusiastic programmer!\u{1F913}', 1000, 'I am a Full Stack Developer!\u{1F4BB}', 500]} 
-                loop={Infinity}
-                />
-            
-            </h3>
-       
-            <p className="hire-me__text">{"Hire Tumelo \u{1F447}"}</p>
-
-            <button className="discover__btn">Discover Me!</button>
-
+                <h3 className="intro__text">
+                    <Typical 
+                    steps={['I am an enthusiastic programmer!\u{1F913}', 1000, 'I am a Full Stack Developer!\u{1F4BB}', 500]} 
+                    loop={Infinity}
+                    />
+                
+                </h3>
+        
+                <div className="hiring">
+                    <p className="hire-me__text">Hire Tumelo</p> 
+                    <span>{"\u{1F447}"}</span>
+                </div>
+                
+                <button className="discover__btn">Discover Me!</button>
+            </div>
         </div>
     )
 }
