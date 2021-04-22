62c36a4a57bbda65c915e106efc4820b4929d411
Tumelo <tumie201511@gmail.com>
2021-04-22 15:12:51 +0200

    modified ResumeView in adding correct class name

--- src/ResumeView/ResumeView.js
+++ src/ResumeView/ResumeView.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import FileViewer from 'react-file-viewer'
 
+
 function ResumeView() {
     return (
         <div className="resume__view">
             <FileViewer fileType={"pdf"} filePath={process.env.PUBLIC_URL + "Resume.pdf"} />
+            
         </div>
     )
 }
