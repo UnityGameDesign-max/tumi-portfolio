64e066af32c5fb7e100c5b211c48251cfff4f674
Tumelo <tumie201511@gmail.com>
2021-04-22 09:15:57 +0200

    modified Resume js file by adding a button to view the pdf resume

--- src/Resume/Resume.js
+++ src/Resume/Resume.js
@@ -7,11 +7,12 @@ function Resume() {
         <div className="resume">
             <h1 className="resume__header">Resume</h1>
             <div className="resume__info">
-                <img className="resume__img" src={process.env.PUBLIC_URL + "Resume.svg"} alt=" "/>
+                <img className="resume__img" src={process.env.PUBLIC_URL + "CV.svg"} alt=" "/>
                 <div className="resume__button__paragraph">
                     <p className="resume__paragraph">This is the Resume of Tumelo Mahlangu that contains all the information about skills, interests and qualifications</p>
-                    <button className="resume__button">View my Resume</button>
+                    <a src={process.env.PUBLIC_URL + "CV.svg"}><button className="resume__button">View my Resume</button></a>
                 </div>
+                
             </div>
             
             
