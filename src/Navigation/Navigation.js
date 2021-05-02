df8a5e330b5cec0ff1839c84ac78a762f98ed005
Tumelo <tumie201511@gmail.com>
2021-05-03 01:48:59 +0200

    added links for the each page

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import './Navigation.css'
+import { Link } from 'react-router-dom'
 
 function Navigation() {
     return (
-        <div className="navigation">
+        <div className="navigation" id="navigation">
             <h3 className="navigation__logo"><a href="/">TumiDev</a></h3>
 
             <nav>
                 <ul className="navigation__link">
-                    <l1 className="navigation__link"><a className="items" href="#">Skills</a></l1>
-                    <l1 className="navigation__link"><a className="items" href="#">Projects</a></l1>
-                    <l1 className="navigation__link"><a className="items" href="#">About</a></l1>
+                    <l1 className="navigation__link"><a className="items" href="#skills">Skills</a></l1>
+                    <l1 className="navigation__link"><a href="#projects" className="items">Projects</a></l1>
+                    <l1 className="navigation__link"><a className="items" href="#about">About</a></l1>
+                    <l1 className="navigation__link"><a className="items" href="#resume">Resume</a></l1>
+                    <l1 className="navigation__link"><a className="items" href="#contacts">Contacts</a></l1>
         
                 </ul>
             </nav>
