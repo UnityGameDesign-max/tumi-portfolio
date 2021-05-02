74002dfc49a7473e22dfb89a16fdc94de512c497
Tumelo <tumie201511@gmail.com>
2021-05-03 01:44:51 +0200

    added Resume js file by replacing a button with a link

--- src/Resume/Resume.js
+++ src/Resume/Resume.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Resume.css'
+import myResume from '../Resume.pdf';
 
 
 
 function Resume() {
     return (
-        <div className="resume">
+        <div className="resume" id="resume">
             <h1 className="resume__header">Resume</h1>
             <div className="resume__info">
                 <img className="resume__img" src={process.env.PUBLIC_URL + "/CV.svg"} alt=" "/>
                 <div className="resume__button__paragraph">
                     <p className="resume__paragraph">This is the Resume of Tumelo Mahlangu that contains all the information about skills, interests and qualifications</p>
-                    <Link to="/resume">
-                        <button className="resume__button">View my Resume</button>
-                    </Link>
+                    <a type="button" className="resume__button" href={myResume} download="Resume.pdf">Download</a>    
+                    
                     
                 </div>
                 
